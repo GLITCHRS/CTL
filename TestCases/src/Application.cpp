52c32699fcc379ee909a432213d9b3c1a3e00216
AMZ <ahmedzaki.bm@gmail.com>
2022-07-12 19:43:05 +0300

    Removed unnecessary two lines of code in main file used for research purposes.

--- TestCases/src/Application.cpp
+++ TestCases/src/Application.cpp
@@ -12,10 +12,6 @@ int main()
 {
 	const CTL::Dynamic::String data{ "Hello World" };
 
-	using namespace std::string_literals;
-
-	""s;
-
 	PrintStr(data);
 	std::cout << data.Index('o') << '\n';
 	std::cout << data.Index("o W") << '\n';
